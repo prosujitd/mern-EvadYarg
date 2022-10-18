1d3c3b37c4e88c12f3954f0e7e49994cb77df16f
Sujit Dahal <prosujit@gmail.com>
2022-10-18 18:47:54 +0545

    chap10 - jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,6 +32,7 @@ const login = asyncHandler(async (req, res) => {
         },
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: '15m' }
+        // { expiresIn: '15s' }
     )
 
     const refreshToken = jwt.sign(
@@ -81,6 +82,7 @@ const refresh = (req, res) => {
                 },
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '15m' }
+                // { expiresIn: '15s' }
             )
 
             res.json({ accessToken })
